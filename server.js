6f740ebe6069d24850b23fe134e6b0b31fc5cb00
Yves Troxler <Yves.troxler@ajila.com>
2023-12-22 09:29:13 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
-const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = socketIO(server);
 
-// Statische Dateien aus dem "public" Verzeichnis servieren
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public')); // Stelle sicher, dass der Server auf den öffentlichen Ordner zugreifen kann
 
-// Websocket-Verbindung herstellen
-wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-        // Broadcast-Nachricht an alle verbundenen Clients senden
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+app.get('/:room', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Empfange Zeichenaktionen vom Client
+    socket.on('draw', (data) => {
+        // Sende die Zeichenaktion an alle anderen Clients im selben Raum
+        socket.broadcast.to(data.room).emit('draw', data);
+    });
+
+    // Trenne den Benutzer, wenn er die Verbindung verliert
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
-// Server auf allen verfügbaren IP-Adressen und dem Port 3000 starten
-const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0';  // Hier auf '0.0.0.0' ändern
-server.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
