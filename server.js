f9f5bab404f0cf9638f8237e4387f04801e443ac
Yves Troxler <Yves.troxler@ajila.com>
2023-12-22 10:53:55 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
 
@@ -6,22 +7,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static('public')); // Stelle sicher, dass der Server auf den öffentlichen Ordner zugreifen kann
+// Stelle sicher, dass der Server auf den öffentlichen Ordner zugreifen kann
+app.use(express.static(path.join(__dirname, 'Whiteboard')));
 
+// Sendet die HTML-Datei für alle Anfragen an den Root-Pfad
 app.get('/:room', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Empfange Zeichenaktionen vom Client
     socket.on('draw', (data) => {
-        // Sende die Zeichenaktion an alle anderen Clients im selben Raum
         socket.broadcast.to(data.room).emit('draw', data);
     });
 
-    // Trenne den Benutzer, wenn er die Verbindung verliert
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
